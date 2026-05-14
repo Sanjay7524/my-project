6f45ca7109724975777bfaf39ffe3e3ae5116f14
Sanjay7524 <133633273+Sanjay7524@users.noreply.github.com>
2026-05-14 13:57:56 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,12 +1,4 @@
-from distutils.command.bdist import bdist
-import sys
+from .more import *  # noqa
+from .recipes import *  # noqa
 
-if 'egg' not in bdist.format_commands:
-    try:
-        bdist.format_commands['egg'] = ('bdist_egg', "Python .egg file")
-    except TypeError:
-        # For backward compatibility with older distutils (stdlib)
-        bdist.format_command['egg'] = ('bdist_egg', "Python .egg file")
-        bdist.format_commands.append('egg')
-
-del bdist, sys
+__version__ = '8.8.0'
